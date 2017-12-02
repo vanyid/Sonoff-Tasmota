0d53f4b88fd83b62796beeb4cd4721d8e131ca04
vanyid <vanyid@gmail.com>
2017-12-02 23:44:10 +0100

    Thermostat funct update based on @nambuco
    Radiator thermostat on, off delay
    different temp sensor source
    Thermostat parameter update trigger the state machine.

--- sonoff/thermostat.ino
+++ sonoff/thermostat.ino
@@ -30,12 +30,13 @@ const char kThermoCommands[] PROGMEM =
     D_CMND_THERMOSTAT "|" D_CMND_SETPOINT ;
 
 enum ThermoStates {
-    STATE_ON, STATE_OFF_DELAY, STATE_OFF, STATE_DISABLED};
+    STATE_ON, STATE_ON_DELAY, STATE_OFF_DELAY, STATE_OFF, STATE_DISABLED};
 
 byte thermo_state = STATE_DISABLED;
 uint8 thermo_timer = 0;
 
 #define OFF_DELAY 90  // minimal off time in seconds
+#define ON_DELAY  90  // minimal on time in seconds
 #define HYSTERESIS 2  // Hysteresys in 0.1 Degrees
 
   
@@ -69,59 +70,95 @@ boolean ThermoCommand(char *type, uint16_t index, char *dataBuf, uint16_t data_l
   else {
     serviced = false;
   }
+
+  if (serviced) {
+    ThermoFunction(0);  // apply changed on the thermostat statemachine
+  }
+
   return serviced;
 }
 
-// Hard Thermostat main function
+// Hard Thermostat main function - tasktime 1s
 void ThermoFunction (int tele_period)
 {
-#ifdef USE_DS18B20
   float temp;
-  thermo_timer ++;
-  if ((pin[GPIO_DSB] < 99) && (tele_period == 0)) {
-    if (Ds18b20ReadTemperature(temp)) {
-//      snprintf_P(log_data, sizeof(log_data), PSTR("Hard Thermostat Function %d | State %d | Temp %s"), Settings.thermo_setpoint, thermo_state, String(temp * 10).c_str());
-//      AddLog(LOG_LEVEL_DEBUG);
-      if (thermo_state == STATE_ON) {
-        if (Settings.thermo == 0) {
-          ExecuteCommandPower(1, 0);
-          thermo_state = STATE_DISABLED;
-        }
-        else if ((temp * 10) < (Settings.thermo_setpoint)) {
-          ExecuteCommandPower(1,0);
-          thermo_timer = 0;
-          thermo_state = STATE_OFF_DELAY;
-        }
-        else {
-          ExecuteCommandPower(1,1);
-        }
+  float humi;
+  bool temp_readed = false;
+  thermo_timer++;
+
+  if (tele_period != 0) {
+    return;
+  }
+  snprintf_P(log_data, sizeof(log_data), PSTR("Teleperiod now"));
+  AddLog(LOG_LEVEL_DEBUG);
+
+#ifdef USE_DS18B20
+  if (pin[GPIO_DSB] < 99) {
+    if (Ds18x20Read(0, temp)) {
+      temp_readed = true;
+    }
+  }
+#endif
+
+#ifdef USE_DHT
+  if (pin[GPIO_DHT11] < 99 || pin[GPIO_DHT21] < 99 || pin[GPIO_DHT22] < 99) {
+    if (DhtReadTempHum(0, temp, humi)) {
+      temp_readed = true;
+      snprintf_P(log_data, sizeof(log_data), PSTR("DHT22 sensor read"));
+      AddLog(LOG_LEVEL_DEBUG);
+    }
+  }
+#endif
+
+  if (Settings.thermo == 0)
+  {
+    ExecuteCommandPower(1, 0);
+    thermo_state = STATE_DISABLED;
+    return;
+  }
+
+  if (temp_readed) {
+    if (thermo_state == STATE_ON) {
+      if ((temp * 10) > (Settings.thermo_setpoint)) {
+        ExecuteCommandPower(1,0);
+        thermo_timer = 0;
+        thermo_state = STATE_OFF_DELAY;
       }
-      else if (thermo_state == STATE_OFF) {
-        if (Settings.thermo == 0) {
-          ExecuteCommandPower(1, 0);
-          thermo_state = STATE_DISABLED;
-        }
-        else if ((temp * 10) > (Settings.thermo_setpoint + HYSTERESIS)) {
-          ExecuteCommandPower(1,1);
-          thermo_state = STATE_ON;
-        }
-        else {
-          ExecuteCommandPower(1,0);
-        }
+      else {
+        ExecuteCommandPower(1,1);
       }
-      else if (thermo_state == STATE_OFF_DELAY) {
-        if (thermo_timer > OFF_DELAY) {
+    }
+    else if (thermo_state == STATE_OFF) {
+      if ((temp * 10) < (Settings.thermo_setpoint - HYSTERESIS)) {
+        ExecuteCommandPower(1,1);
+        thermo_state = STATE_ON_DELAY;
+        thermo_timer = 0;
+      }
+      else {
+        ExecuteCommandPower(1,0);
+      }
+    }
+    else if (thermo_state == STATE_OFF_DELAY) {
+      if (thermo_timer > OFF_DELAY) {
+      thermo_state = STATE_OFF;
+      thermo_timer = 0;
+      }
+    }
+    else if (thermo_state == STATE_ON_DELAY) {
+      if (thermo_timer > ON_DELAY) {
+      thermo_state = STATE_ON;
+      thermo_timer = 0;
+      }
+    }
+    else if (thermo_state == STATE_DISABLED) {
+      if (Settings.thermo == 1) {
+        ExecuteCommandPower(1, 0);
         thermo_state = STATE_OFF;
         thermo_timer = 0;
-        }
-      }
-      else if (thermo_state == STATE_DISABLED) {
-        if (Settings.thermo == 1) {
-          ExecuteCommandPower(1, 0);
-          thermo_state = STATE_OFF;
-        }
+        ThermoFunction(0);
       }
     }
   }
-#endif  // USE_DS18B20
+  snprintf_P(log_data, sizeof(log_data), PSTR("Thermostat state %d"), thermo_state);
+  AddLog(LOG_LEVEL_DEBUG);
 }
